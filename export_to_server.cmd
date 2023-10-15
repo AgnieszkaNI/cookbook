40971a843dc02443d9fb4201f1f7b532936bb782
Agnieszka Nowak <agnieszka.nowak.box@gmail.com>
2023-10-15 13:36:15 +0200

    Added cookbook sources

---
+++ export_to_server.cmd
@@ -0,0 +1,5 @@
+:: Sktypt bash wysyłąjący stronę na serwer
+:: Należy klikną 2 razy w plik w folderze (w Eksploratorze plików) lub
+:: wywołać skrypt wpisując go w Terminal (ctrl+shift+`) i Enter
+xcopy index.html W:\cookbook\ /Y
+xcopy recips.css W:\cookbook\ /Y
